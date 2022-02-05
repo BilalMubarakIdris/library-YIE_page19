a31873c09fb9305d4b295d36abdd9d71d63300e3
CodeWithBilal <bilalmubarakidris@gmail.com>
2022-02-05 14:49:36 +0530

    pushing code to git

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+export default function Menu(){
+    return (
+        <div className="border-solid border-2  h-screen">
+            <ul className="list-none p-0">
+                <li className="bg-gray-800 py-2 px-0 my-0.5">
+                <h3 className="py-0 px-20 text-white">Library</h3>
+                </li>
+            </ul>
+        </div>
+    )
+}
